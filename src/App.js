f9eccf135ef62e99420d754a0bd723185f59475c
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-22 18:25:00 +0100

    Start with redux

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import './App.css';
 import Navbar from './containers/navbar/Navbar';
 import BookPage from './pages/books/BookPage';
 import CategoriesPage from './pages/categories/CategoriesPage';
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<BookPage />} />
-      <Route path="/categories" element={<CategoriesPage />} />
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
