00d51683e40ac11e4aa1156e788d4b10a101db09
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-25 19:32:00 +0100

    Set onClick event handler + Set proptype

--- src/components/addNBButton/AddNBButton.js
+++ src/components/addNBButton/AddNBButton.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 import './AddNBButton.css';
 
-const AddNBButton = () => (
-  <button type="button" className="addBook">ADD BOOK</button>
-);
+const AddNBButton = (props) => {
+  const { handleClick } = props;
+  return (
+    <button
+      type="button"
+      className="addBook"
+      onClick={handleClick}
+    >
+      ADD BOOK
+    </button>
+  );
+};
 
 export default AddNBButton;
+
+AddNBButton.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  // eslint-disable-next-line react/no-typos
+  handleClick: PropTypes.func,
+};
+
+AddNBButton.defaultProps = {
+  // eslint-disable-next-line react/require-default-props
+  // eslint-disable-next-line react/no-typos
+  handleClick: () => { },
+};
