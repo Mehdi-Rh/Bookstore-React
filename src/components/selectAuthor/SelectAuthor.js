4f3907b5b5201a64d942ab93522566cda29e9dc4
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-25 22:00:05 +0100

    Set placeholder instead of defaultValue

--- src/components/selectAuthor/SelectAuthor.js
+++ src/components/selectAuthor/SelectAuthor.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import './SelectAuthor.css';
-// import Dropdown from 'react-bootstrap/Dropdown';
-// import DropdownButton from 'react-bootstrap/DropdownButton';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const SelectAuthor = (props) => {
   const { handleChangeAuthor } = props;
   return (
-  // <Dropdown onSelect={handleChangeAuthor}>
-  //   <Dropdown.Toggle variant="Info" id="lessonPanel">
-  //     <span id="titleCategory">{newAuthor}</span>
-  //   </Dropdown.Toggle>
-
-    //   <Dropdown.Menu>
-    //     <Dropdown.Item href="#/action-1">Author_1</Dropdown.Item>
-    //     <Dropdown.Item href="#/action-2">Author_2</Dropdown.Item>
-    //     <Dropdown.Item href="#/action-3">Author_3</Dropdown.Item>
-    //   </Dropdown.Menu>
-    // </Dropdown>
     <div>
       <select
         id="fruits"
-        defaultValue="Select fruit"
+        placeholder="Select Author"
         onChange={handleChangeAuthor}
       >
+        <option value="none" selected disabled hidden>Select Author</option>
         <option value="Author_1">Author_1</option>
         <option value="Author_2">Author_2</option>
         <option value="Author_3">Author_3</option>
@@ -39,13 +28,10 @@ SelectAuthor.propTypes = {
   // eslint-disable-next-line react/require-default-props
   // eslint-disable-next-line react/no-typos
   handleChangeAuthor: PropTypes.func,
-  // newAuthor: PropTypes.string,
 };
 
 SelectAuthor.defaultProps = {
   // eslint-disable-next-line react/require-default-props
   // eslint-disable-next-line react/no-typos
   handleChangeAuthor: () => { },
-  // newAuthor: 'Select Author',
-
 };
