bf21bb4c10ac56fca3c1efbbe31f1178465ff1b8
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-25 19:39:43 +0100

    Set the categories checkStatus

--- src/pages/categories/CategoriesPage.js
+++ src/pages/categories/CategoriesPage.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatusAction } from '../../redux/categories/categories';
 
-const CategoriesPage = () => (
-  <button type="button">Check status</button>
-);
+const CategoriesPage = () => {
+  const status = useSelector((state) => state.checkStatus);
+  const dispatch = useDispatch();
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(checkStatusAction());
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick} type="button">Check status</button>
+      {status && <h2>{status}</h2>}
+
+    </div>
+
+  );
+};
 
 export default CategoriesPage;
